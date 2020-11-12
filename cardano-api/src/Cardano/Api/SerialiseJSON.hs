89232ce606a2886b66eafc6516956bbe01931f0b
Jordan Millar <jordan.millar@iohk.io>
2020-11-12 13:22:08 +0000

    Add prettyPrintJSON function

--- cardano-api/src/Cardano/Api/SerialiseJSON.hs
+++ cardano-api/src/Cardano/Api/SerialiseJSON.hs
@@ -5,6 +5,7 @@ module Cardano.Api.SerialiseJSON
   ( serialiseToJSON
   , ToJSON(..)
   , deserialiseFromJSON
+  , prettyPrintJSON
   , FromJSON(..)
   , JsonDecodeError(..)
   ) where
@@ -14,10 +15,10 @@ import           Prelude
 import           Data.ByteString (ByteString)
 import qualified Data.ByteString.Lazy as LBS
 
+import           Cardano.Api.HasTypeProxy
 import           Data.Aeson (FromJSON (..), ToJSON (..))
 import qualified Data.Aeson as Aeson
-
-import           Cardano.Api.HasTypeProxy
+import           Data.Aeson.Encode.Pretty (encodePretty)
 
 
 newtype JsonDecodeError = JsonDecodeError String
@@ -25,7 +26,8 @@ newtype JsonDecodeError = JsonDecodeError String
 serialiseToJSON :: ToJSON a => a -> ByteString
 serialiseToJSON = LBS.toStrict . Aeson.encode
 
---TODO: add prettyPrintJSON :: ToJSON a => a -> ByteString
+prettyPrintJSON :: ToJSON a => a -> ByteString
+prettyPrintJSON = LBS.toStrict . encodePretty
 
 deserialiseFromJSON :: FromJSON a
                     => AsType a
